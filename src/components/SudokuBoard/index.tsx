5612995d0ef58ef3a3d0db934433f6be8f97770f
kaiovbraga <kaiovbraga2001@gmail.com>
2021-06-12 00:10:17 -0300

    style: linting

--- src/components/SudokuBoard/index.tsx
+++ src/components/SudokuBoard/index.tsx
@@ -1,50 +1,60 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback } from "react";
 import { Container, SubContainer, SquareContainer } from "./styles";
 
 const SudokuBoard = () => {
-  const subContainers = Array(9).fill(null).map((v:null, index) => index);
-  const lineContainer = Array(3).fill(null).map((v:null, index) => index);
-  const [boardElements, setBoardElements] = useState(Array(81).fill(null).map((v:null, index) => ({
-    value: index + 1,
-    display: true
-  })));
+  const subContainers = Array(9)
+    .fill(null)
+    .map((v: null, index) => index);
+  const lineContainer = Array(3)
+    .fill(null)
+    .map((v: null, index) => index);
+  const [boardElements, setBoardElements] = useState(
+    Array(81)
+      .fill(null)
+      .map((v: null, index) => ({
+        value: index + 1,
+        display: true,
+      }))
+  );
 
-  const handleToggle = useCallback((index) => {
-    const newBoardElements = [...boardElements];
-    
-    const {display} = newBoardElements[index];
-    newBoardElements[index] = {...newBoardElements[index], display: !display};
+  const handleToggle = useCallback(
+    (index) => {
+      const newBoardElements = [...boardElements];
 
-    setBoardElements(newBoardElements);
-  }, [boardElements]);
+      const { display } = newBoardElements[index];
+      newBoardElements[index] = {
+        ...newBoardElements[index],
+        display: !display,
+      };
+
+      setBoardElements(newBoardElements);
+    },
+    [boardElements]
+  );
 
   return (
     <Container>
+      {subContainers.map((sub) => {
+        const line = 3 * (sub % 3) + Math.floor(sub / 3) * 27;
 
-      {subContainers.map(sub => {
-        const line = 3*(sub%3) + Math.floor(sub/3) * 27;
-
-        return(
+        return (
           <SubContainer>
-            {lineContainer.map(v => {
-              const start = (v%3)*9 + line;
-              const end = (v%3)*9 + line + 3;
+            {lineContainer.map((v) => {
+              const start = (v % 3) * 9 + line;
+              const end = (v % 3) * 9 + line + 3;
 
-              return(
-                boardElements
+              return boardElements
                 .slice(start, end)
-                .map(
-                  ({value, display}, index) => <SquareContainer onClick={() => handleToggle(start+index)}>{display ? value : null}</SquareContainer>
-                )
-              );
+                .map(({ value, display }, index) => (
+                  <SquareContainer onClick={() => handleToggle(start + index)}>
+                    {display ? value : null}
+                  </SquareContainer>
+                ));
             })}
           </SubContainer>
         );
       })}
-
-     
     </Container>
-    
   );
 };
 
